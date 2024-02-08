78c0ac148cc521f53642e779c3e6dce7c6acd3ad
Jiaying Qiu <elaineqiu0531@gmail.com>
2024-02-08 16:09:54 -0500

    "Task4: fix ViewBinding"

--- app/src/main/java/com/college/converter/MainActivity.java
+++ app/src/main/java/com/college/converter/MainActivity.java
@@ -33,7 +33,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //implement binding
+        variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
 
         Button buttonConvert = variableBinding.convertButton;
 
